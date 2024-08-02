629ed8d53e6a5a72a8591f536a59ffd752058271
Tamilarasan-ASE <tamilrk2002@gmail.com>
2024-08-02 18:48:04 +0530

    Whole Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import FirstPage from "./pages/FirstPage";
+import Secondpage from "./pages/Secondpage";
+import RenderingPage from "./pages/RenderingPage";
+import Form from "./pages/Form";
+import NavigationConfigPage from "./pages/NavigationConfigPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div class="w-full min-h-screen h-full">
+        <Routes>
+          <Route path="/" element={<FirstPage />} />
+          <Route path="/create" element={<Secondpage />} />
+          <Route path="/create/pages/:wizardId" element={<RenderingPage />} />
+          <Route path="/edit/pages/:wizardId" element={<RenderingPage />} />
+          <Route path="/edit/:templateId" element={<Secondpage />} />
+          <Route
+            path="/edit/:templateId/:wizardId"
+            element={<RenderingPage />}
+          />
+          <Route
+            path="/preview/:templateId/:wizardId"
+            element={<RenderingPage />}
+          />
+          <Route path="/form/:templateId/:wizardId" element={<Form />} />
+          <Route path="/create/navConfig" element={<NavigationConfigPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
